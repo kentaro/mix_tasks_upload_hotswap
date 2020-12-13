93411bc618706654b14db036d788acab21d880ec
Kentaro Kuribayashi <kentarok@gmail.com>
2020-12-13 11:39:41 +0900

    add metadata for package

--- mix.exs
+++ mix.exs
@@ -7,16 +7,14 @@ defmodule Mix.Tasks.Upload.Hotswap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      xref: [exclude: IEx.Helpers],
-
-      # Docs
       name: "mix upload.hotswap",
       source_url: "https://github.com/kentaro/mix_tasks_upload_hotswap",
       homepage_url: "https://github.com/kentaro/mix_tasks_upload_hotswap",
-      docs: [
-        main: "Mix.Tasks.Upload.Hotswap",
-      ]
+      description: description(),
+      deps: deps(),
+      xref: [exclude: IEx.Helpers],
+      package: package(),
+      docs: docs(),
     ]
   end
 
@@ -33,6 +31,23 @@ defmodule Mix.Tasks.Upload.Hotswap.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    "Deploy local code changes to the remote node(s) in a hot-code-swapping manner."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kentaro/mix_tasks_upload_hotswap"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Mix.Tasks.Upload.Hotswap",
     ]
   end
 end
